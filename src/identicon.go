69e4e6552223fee686b789a6605fcbcf855eaa2b
Jonathan Gimeno <jgimeno@gmail.com>
2018-01-17 23:58:32 +0100

    Add render function.

--- src/identicon.go
+++ src/identicon.go
@@ -2,22 +2,34 @@ package identicon
 
 import (
 	"crypto/md5"
+	"image/color"
 )
 
 type Identicon struct {
 	bitmap []byte
+	color color.Color
 }
 
 func Generate(key string) Identicon {
 	hash := md5.Sum([]byte(key))
 	return Identicon{
 		convertPatternToBinarySwitch(generatePatternFromHash(hash)),
+		getColorFromHash(hash),
+	}
+}
+
+func getColorFromHash(h [16]byte) color.Color {
+	lastBytes := h[13:]
+	return color.RGBA{
+		R: lastBytes[0],
+		G: lastBytes[1],
+		B: lastBytes[2],
+		A: 255,
 	}
 }
 
 func generatePatternFromHash(sum [16]byte) []byte {
 	p := make([]byte, 25)
-
 	for i := 0; i < 5; i++ {
 		for j := 0; j < 5; j++ {
 			jCount := j
@@ -29,7 +41,6 @@ func generatePatternFromHash(sum [16]byte) []byte {
 			p[5 * i + j] = sum[3 * i + jCount]
 		}
 	}
-
 	return p
 }
 
@@ -42,6 +53,5 @@ func convertPatternToBinarySwitch(pattern []byte) []byte {
 			b[i] = 0
 		}
 	}
-
 	return b
 }
--- src/renderer.go
+++ src/renderer.go
@@ -1,1 +1,15 @@
 package identicon
+
+import (
+	"image"
+	"os"
+	"image/png"
+)
+
+func Render(i Identicon) {
+	img := image.NewRGBA(image.Rect(0,0, 50, 50))
+
+	f, _ := os.OpenFile("out.png", os.O_WRONLY|os.O_CREATE, 0600)
+	defer f.Close()
+	png.Encode(f, img)
+}
